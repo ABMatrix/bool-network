6e9a7eca6ba0f5b7b1198526bf1b546bb4ada2a2
Jason <luqz812@126.com>
2019-09-21 20:16:01 +0800

    add shell script

---
+++ caster/testscript.sh
@@ -0,0 +1,1 @@
+../target/release/caster tx -k 0x4db4ef1992889d4428e400be3428843db6e89bb2e8aaf4ce8efe00df64012544 -m publish --compiled_file ./scripts/s.mvir -s 2
---
+++ caster/testtransfer.sh
@@ -0,0 +1,1 @@
+../target/release/caster tx -m transfer -k 0x4db4ef1992889d4428e400be3428843db6e89bb2e8aaf4ce8efe00df64012544 -r 0x01add5624932fc6e5e82ea4b8b4217c2ea4372a1e4fbc9d910a38b2514931166 -v 100 -s 0
