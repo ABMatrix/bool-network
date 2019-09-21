cb5bbf00b4f8cad774b05a2ff960f8f4b28c17f3
Jason <luqz812@126.com>
2019-09-21 20:29:08 +0800

    add comment in testscript.sh

--- caster/testscript.sh
+++ caster/testscript.sh
@@ -1,1 +1,2 @@
+# use script transfer coin to 0x0000000000000000000000000000000000000000000000000000000000000001
 ../target/release/caster tx -k 0x4db4ef1992889d4428e400be3428843db6e89bb2e8aaf4ce8efe00df64012544 -m publish --compiled_file ./scripts/s.mvir -s 2
